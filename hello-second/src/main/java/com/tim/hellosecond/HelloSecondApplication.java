2d6ed3fa41d7c4dc208dd13240563284503a46f8
Tim Löfström <lo.tim@hotmail.com>
2020-11-20 01:42:01 +0100

    working example

---
+++ hello-second/src/main/java/com/tim/hellosecond/HelloSecondApplication.java
@@ -0,0 +1,13 @@
+package com.tim.hellosecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSecondApplication.class, args);
+	}
+
+}
---
+++ hello-second/src/test/java/com/tim/hellosecond/HelloSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.hellosecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
